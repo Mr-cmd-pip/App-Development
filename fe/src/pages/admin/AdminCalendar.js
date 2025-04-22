777a0425e1e5db66409cdf6ca1cb5498753388dc
Matthew Rimar Martus <matthewrimar1102@gmail.com>
2025-04-22 18:51:17 +0800

    Added Calendar for Admin

--- fe/src/pages/admin/AdminCalendar.js
+++ fe/src/pages/admin/AdminCalendar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import "../../css/Sidebar.css";
 import Sidebar from '../../components/SideBar';
+import Calendar from '../../components/Calendar';
 
 function AdminCalendar() {
     return (
-        <div style={{ display: 'flex', height: '100vh' }}>
-        <Sidebar />
-        <div style={{ flex: 1, padding: '20px' }}>
-          <h1>Hello from Calendar</h1>
+        <div style={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
+            <Sidebar />
+            <div style={{ flex: 1, padding: '20px', overflow: 'hidden' }}>
+                <div style={{ height: '100%', width: '100%', overflow: 'hidden' }}>
+                    <Calendar />
+                </div>
+            </div>
         </div>
-      </div>
     )
 }
 
